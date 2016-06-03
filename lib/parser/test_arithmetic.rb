b7db4ffa7517b51d3a47c45f99a055dc0076abd0
Greg Stanley <gstanley@frii.com>
2016-06-03 17:30:22 -0600

    add helper class

--- lib/parser/test_arithmetic.rb
+++ lib/parser/test_arithmetic.rb
@@ -4,13 +4,15 @@ require "#{dir}/test_helper"
 Treetop.load "#{dir}/arithmetic"
 
 class ArithmeticGrammarTest < Test::Unit::TestCase
+  include ParserTestHelper
+
   def setup
     @parser = ArithmeticParser.new
   end
 
   test "parse numbers" do
-    assert_equal 0, @parser.parse("0").eval
-    assert_equal 123, @parser.parse("123").eval
+    assert_equal 0, parse("0").eval
+    assert_equal 123, parse("123").eval
   end
 end
 
