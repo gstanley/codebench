dce0d56e46add0f139392fa395e4d557defcc39e
Greg Stanley <gstanley@frii.com>
2016-06-13 16:30:22 -0600

    misc stuff

---
+++ lib/misc/mdate.rb
@@ -0,0 +1,16 @@
+class MDate
+  # load
+  #   original object
+  #   structure
+  #   wrapped object
+  def self.load(*args)
+    
+  end
+
+  def method_missing(*args, &block)
+  end
+
+  def to_structure
+  end
+end
+
---
+++ lib/misc/readme.txt
@@ -0,0 +1,1 @@
+These files are not part of the codebench system. This is just a place to put some miscellaneous code.
