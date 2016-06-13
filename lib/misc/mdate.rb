9f1c542c3fa77166399f26cc8c66b318fad80612
Greg Stanley <gstanley@frii.com>
2016-06-13 17:04:51 -0600

    stuff for treating global variable

--- lib/misc/mdate.rb
+++ lib/misc/mdate.rb
@@ -14,3 +14,28 @@ class MDate
   end
 end
 
+$x = nil
+class GlobalXVar
+  def getx
+    $x
+  end
+
+  def setx(new_x)
+    $x = new_x
+    nil
+  end
+end
+
+class MGlobalVar
+  def self.load(*args)
+  end
+
+  def method_missing(*args, &block)
+  end
+
+  def to_structure
+  end
+end
+
+# debugger utilities
+
