0c1809d76df8fcffe437529b1884d7140e19f625
Greg Stanley <gstanley@frii.com>
2016-06-15 17:16:47 -0600

    add openstruct style for trees

--- lib/parser/test_ruby.rb
+++ lib/parser/test_ruby.rb
@@ -12,14 +12,14 @@ class RubyGrammarTest < Test::Unit::TestCase
 
   test "parse expression" do
     result = parse("1.class").tree
-    assert_equal "1", result[:method_call][:primary_value]
-    assert_equal "class", result[:method_call][:operation2]
+    assert_equal "1", result.method_call.primary_value.text
+    assert_equal "class", result.method_call.operation2.text
   end
 
   test "parse expression with comment and float" do
     result = parse("0.0.class # => Float: floating-point numbers have class Float").tree
-    assert_equal "0.0", result[:method_call][:primary_value]
-    assert_equal "class", result[:method_call][:operation2]
-    assert_equal " => Float: floating-point numbers have class Float", result[:method_call][:comment]
+    assert_equal "0.0", result.method_call.primary_value.text
+    assert_equal "class", result.method_call.operation2.text
+    assert_equal " => Float: floating-point numbers have class Float", result.method_call.comment.text
   end
 end
