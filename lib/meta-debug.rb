bd7266fccffd346e0a1f8d82597e25527c4b2682
Greg Stanley <gstanley@frii.com>
2016-07-14 16:56:34 -0600

    add code

--- lib/meta-debug.rb
+++ lib/meta-debug.rb
@@ -4,10 +4,16 @@ $trace = []
 Pry::Commands.create_command "trace" do
   description "Trace the state."
 
+  def setup
+    @file = File.expand_path(target.eval('__FILE__'))
+    @line = target.eval('__LINE__')
+  end
+
   def process
     $trace << {
-      file: File.expand_path(target.eval('__FILE__')),
-      line: target.eval('__LINE__')
+      file: @file,
+      line: @line,
+      code: Pry::Code.from_file(@file).take_lines(@line, 1).to_s
     }
   end
 end
