979d4cf11d14a353816924d4118c46d20e25d744
Greg Stanley <gstanley@frii.com>
2016-07-14 16:27:10 -0600

    add line and file to trace show-trace

--- lib/meta-debug.rb
+++ lib/meta-debug.rb
@@ -1,9 +1,20 @@
 # first move this code into .pryrc
 
+$trace = []
 Pry::Commands.create_command "trace" do
   description "Trace the state."
 
   def process
-    output.puts "Tracing line..."
+    $trace << {
+      file: File.expand_path(target.eval('__FILE__')),
+      line: target.eval('__LINE__')
+    }
+  end
+end
+Pry::Commands.create_command "show-trace" do
+  description "Show the trace."
+
+  def process
+    output.puts $trace.inspect
   end
 end
