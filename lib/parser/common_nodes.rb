c9788ee00e380c87860e173df631704d02c0cb4c
Greg Stanley <gstanley@frii.com>
2016-09-08 17:07:48 -0600

    use common nodes

---
+++ lib/parser/common_nodes.rb
@@ -0,0 +1,22 @@
+module Common
+  class Identifier < Treetop::Runtime::SyntaxNode
+  end
+
+  class AssignmentOperator < Treetop::Runtime::SyntaxNode
+  end
+
+  class IntegerLiteral < Treetop::Runtime::SyntaxNode
+  end
+
+  class Expression < Treetop::Runtime::SyntaxNode
+  end
+
+  class Assignment < Treetop::Runtime::SyntaxNode
+  end
+
+  class Statement < Treetop::Runtime::SyntaxNode
+  end
+
+  class Block < Treetop::Runtime::SyntaxNode
+  end
+end
